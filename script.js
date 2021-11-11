f0d109f5fcd053d7fedb455c447d4814eb6f8a49
Nathan Helms <nathanhelms93@gmail.com>
2021-11-11 00:43:51 -0600

    html structure adjustments

---
+++ script.js
@@ -0,0 +1,14 @@
+var cityName = "THIS IS A TEST"
+var weatherConditionsDom = `
+<div>
+    <h1><var>cityName</var></h1>
+</div>
+`;
+
+function test() {
+    weatherConditionsDom 
+    console.log("string")
+}
+
+test()
+
